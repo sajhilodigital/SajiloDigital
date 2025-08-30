3781a3286d1816b353d15d9641445924ad810798
BalGobind Chaudhary <gobind9800@gmail.com>
2025-08-30 20:40:20 +0545

    Changes

--- sajilodigital/src/app/layout.tsx
+++ sajilodigital/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-amber-300`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
         <ThemeProvider>
           <Navbar />
--- sajilodigital/src/app/page.tsx
+++ sajilodigital/src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen w-full">
+    <main className="w-full flex items-center justify-center min-h-screen ">
       <section>
         Hello HeroSection
       </section>
